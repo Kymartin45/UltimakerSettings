d5ef2d88f0e96befad202edc79edec1a3b0101c4
Kyle Martin <kymartin@csumb.edu>
2021-02-06 17:16:45 -0800

    Slight string changes

--- curaSettings.py
+++ curaSettings.py
@@ -1,32 +1,34 @@
 import csv
 from os import name, write
-import datetime
+from datetime import date
 
-today = datetime.datetime.now()
+today = date.today()
 print('Todays date:', today.strftime('%x'))
 
 print('Ultimaker Cura Printer Settings. Input settings below!')
 
-def settings_name():
-    print('\nType your name')
-    setName = input('Name: ')
+# def settings_name():
+#     print('\nType your name')
+#     setName = input('Name: ')
 
-    print(f'\nThanks {setName}! Lets get started\n')
+#     print(f'\nThanks {setName}! Lets get started\n')
 
-    nameDict = {}
-    while True: 
-        save = input('Do you want to save your name? Y or N? ')
-        if save.lower() == 'y':
-            nameDict['name'] = setName
-            print('Name saved!\n\n')
-            break
-        elif save.lower() == 'n':
-            print('Okay, lets continue\n\n')
-            break
-        else: 
-            print('Please type y or n. Goodbye')
-            exit()
-settings_name()
+#     nameDict = {}
+    # while True: 
+        # if save.lower() == {save}:
+        #     nameDict['name'] = setName
+        #     print('Name saved!\n\n')
+        #     break
+        # elif save.lower() == 'n':
+        #     print('Okay, lets continue\n\n')
+        #     break
+        # else: 
+        #     print('Please type y or n. Goodbye')
+        #     exit()
+# settings_name()
+
+# Allows user to configure file name
+settingsName = input('Give your file a name: ')
 
 print('-------Ultimaker Cura Printer Settings-------')
 printTemp = input('Printing Tempterature: ')
@@ -41,7 +43,7 @@ retractExtraPrimeAmt = input('Retraction Extra Prime Amount: ')
 combingMode = input('Combing Mode: ')
 
 '''writes data to .csv file'''
-with open('curaSettings.csv', 'w+') as file:
+with open('{}.csv'.format(settingsName), 'w+') as file:
     myData = csv.writer(file)
     myData.writerow([
         'Printing Temperature',
@@ -67,17 +69,4 @@ with open('curaSettings.csv', 'w+') as file:
         retractExtraPrimeAmt,
         combingMode
     ])
-    print('\nDownloading .csv file. Enjoy!')
-
-appendSettings = [
-    'Printing Temperature',
-    'Build Plate Temperature',
-    'Flow (%)',
-    'Printing Speed (mm/s)',
-    'Enable Retraction',
-    'Retraction at Layer Change',
-    'Retraction Distance',
-    'Retraction Speed (mm/s)',
-    'Retraction Extra Prime Amount (mm)',
-    'Combing Mode'
-]
+    print('\nDownloading {}.csv file. Enjoy!'.format(settingsName))
